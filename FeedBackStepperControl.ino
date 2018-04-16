3fa911836b306c5e3bb8f48d7573bf6c7f290f91
Andrew Harris <andrew.harris@subwaypro.com>
2018-04-16 15:14:22 -0400

    Update - working control
    Stepper is now controllable via I2C.

--- FeedBackStepperControl.ino
+++ FeedBackStepperControl.ino
@@ -1,31 +1,80 @@
-#define pin_STEP 6
-#define pin_DIR 7
+#include <I2C_Anything.h>
+
+#include <Wire.h>
+#include <DigitalIO.h>
+
+#define pin_STEP 11
+#define pin_DIR 12
+#define pinA 2
+#define pinB 3
+
+PinIO readPinA(pinA);
+PinIO readPinB(pinB);
+
+int counter;
+
+bool runOnce = true;
+
+unsigned long lastPulseMicros = 0;
+unsigned long pulseDelayMicros = 2000;
+
+long distanceToTarget = 0;
+volatile int targetEncoderPosition = 199;
 
 void setup() {
   // put your setup code here, to run once:
+  //Serial.begin(9600);
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+//  Serial.begin(9600);
 
+  pinMode(pinA, INPUT);
+  pinMode(pinB, INPUT);
+  attachInterrupt(0, pinA_ISR, RISING);
+  attachInterrupt(1, pinB_ISR, RISING);
+  counter = 0;
   //Sets stepper control pins to outputs
   pinMode(pin_STEP, OUTPUT);
   pinMode(pin_DIR, OUTPUT);
+  distanceToTarget = targetEncoderPosition - counter;
+//  Serial.println(distanceToTarget);
 }
+
+void pinA_ISR(){
+  //If the other pin is low, pin A is leading
+  if(readPinB.read() == 0){
+    counter--; //increments the times that pin A leads
+  }
+}
+
+void pinB_ISR(){
+  if(readPinA.read() == 0){
+    counter++; //Increments the times that pin B leads
+  }
+  
+}
+
+
+
+void receiveEvent(int howMany) {
+  I2C_readAnything(targetEncoderPosition);
+  targetEncoderPosition = (int)(targetEncoderPosition * 1.111111);
+}
+
 
 void loop() {
-  stepMotor(100, true);
+  distanceToTarget = targetEncoderPosition - counter;
+//  Serial.println(targetEncoderPosition);
   // put your main code here, to run repeatedly:
-
-}
-
-void stepMotor(int steps, boolean dir){
-  if(dir){ //Sets direction  
-  digitalWrite(pin_DIR, HIGH); 
-  }
-  else{
-    digitalWrite(pin_DIR, LOW);
-  }
-  for(int i = 0 ; i < steps; i++){ //Pulses for prescribed number of steps
+  if(abs(distanceToTarget) > 1){
+    if(distanceToTarget > 0){
+      digitalWrite(pin_DIR, LOW);
+    }
+    else{
+      digitalWrite(pin_DIR, HIGH);
+    }
     digitalWrite(pin_STEP, HIGH);
     digitalWrite(pin_STEP, LOW);
-    delay(1);
+    delayMicroseconds(125);
   }
 }
-
