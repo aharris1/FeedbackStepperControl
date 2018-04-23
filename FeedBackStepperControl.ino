3122519fc72a7ddb0813635d0578a20a7df018ad
Andrew Harris <andrew.harris@subwaypro.com>
2018-04-23 12:59:47 -0400

    Simplification
    Stripped out much of the normal operating code so only the feature could be tested.

--- FeedBackStepperControl.ino
+++ FeedBackStepperControl.ino
@@ -13,12 +13,7 @@
 #define pinA 2
 #define pinB 3
 
-PinIO readPinA(pinA);
-PinIO readPinB(pinB);
-
 int counter;
-
-bool runOnce = true;
 
 unsigned long lastPulseMicros = 0;
 unsigned long pulseDelayMicros = 2000;
@@ -28,17 +23,12 @@ volatile int targetEncoderPosition = 199;
 volatile int rpm = 0;
 float VESCrpm = 0;
 void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(9600);
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onReceive(receiveEvent); // register event
-//  Serial.begin(9600);
-
   pinMode(pinA, INPUT);
   pinMode(pinB, INPUT);
   attachInterrupt(0, pinA_ISR, RISING);
   attachInterrupt(1, pinB_ISR, RISING);
   counter = 0;
+  Serial.begin(9600);
   //Sets stepper control pins to outputs
   pinMode(pin_STEP, OUTPUT);
   pinMode(pin_DIR, OUTPUT);
@@ -50,46 +40,19 @@ void setup() {
 
 void pinA_ISR(){
   //If the other pin is low, pin A is leading
-  if(readPinB.read() == 0){
+  if(!DigitalPin.fastDigitalRead(pinB)){
     counter--; //increments the times that pin A leads
   }
 }
 
 void pinB_ISR(){
-  if(readPinA.read() == 0){
+  if(!DigitalPin.fastDigitalRead(pinA)){
     counter++; //Increments the times that pin B leads
   }
   
 }
 
 
-
-void receiveEvent(int howMany) {
-  I2C_readAnything(targetEncoderPosition);
-  I2C_readAnything(rpm);
-  VESCrpm = (rpm - 127) / 18.1428 
-  targetEncoderPosition = (int)(targetEncoderPosition * 1.111111);
-}
-
-
 void loop() {
-  distanceToTarget = targetEncoderPosition - counter;
-//  Serial.println(targetEncoderPosition);
-  // put your main code here, to run repeatedly:
-  if(micros()-lastPulseMicros > 500)
-  {
-    if(abs(distanceToTarget) > 1){
-      if(distanceToTarget > 0){
-        digitalWrite(pin_DIR, LOW);
-      }
-      else{
-        digitalWrite(pin_DIR, HIGH);
-      }
-      digitalWrite(pin_STEP, HIGH);
-      delayMicroseconds(1);
-      digitalWrite(pin_STEP, LOW);
-      lastPulseMicros=micros();
-    }
-  }
-  
+  Serial.println(counter);
 }
