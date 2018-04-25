7181cc4f32c5587dd014c90b9e13882d9c22966f
Andrew Harris <andrew.harris@subwaypro.com>
2018-04-24 21:06:20 -0400

    Nearly works: skips steps every now and then but mostly controllable

--- FeedBackStepperControl.ino/FeedBackStepperControl.ino.ino
+++ FeedBackStepperControl.ino/FeedBackStepperControl.ino.ino
@@ -1,6 +1,4 @@
-#include "VescUart.h"
-#include "datatypes.h"
-#include "local_datatypes.h"
+
 
 
 #include <I2C_Anything.h>
@@ -13,7 +11,7 @@
 #define pinA 2
 #define pinB 3
 
-int counter;
+volatile int counter;
 int stepper = 0;
 
 bool runOnce = true;
@@ -28,6 +26,11 @@ volatile int rpm = 0;
 int rollingAvg = 0;
 int rollingAvgInterval = 5; //number of measurements for rolling average to span
 float VESCrpm = 0;
+
+  
+volatile int counterA = 0;
+volatile int counterB = 0;
+
 void setup() {
   // put your setup code here, to run once:
   //Serial.begin(9600);
@@ -36,38 +39,39 @@ void setup() {
 
   fastPinMode(pinA, INPUT);
   fastPinMode(pinB, INPUT);
+
+  fastPinMode(pin_STEP, OUTPUT);
+  fastPinMode(pin_DIR, OUTPUT);
   
   attachInterrupt(0, pinA_ISR, RISING);
-//  attachInterrupt(1, pinB_ISR, RISING);
+  attachInterrupt(1, pinB_ISR, RISING);
   counter = 0;
   //Sets stepper control pins to outputs
-  pinMode(pin_STEP, OUTPUT);
-  pinMode(pin_DIR, OUTPUT);
   digitalWrite(A4, LOW);
   digitalWrite(A5, LOW);
   distanceToTarget = targetEncoderPosition - counter;
   //  Serial.println(distanceToTarget);
+
+  counterA = 0;
+  counterB = 0;
 }
 
-void pinA_ISR() {
+void pinA_ISR(){
   //If the other pin is low, pin A is leading
   if (!fastDigitalRead(pinB)) {
     counter--; //increments the times that pin A leads
   }
-  else
-  {
-    counter++;
-  }
+  counterA++;
 }
 
 
-//
-//  void pinB_ISR(){
-//  if(readPinA.read() == 0){
-//    counter++; //Increments the times that pin B leads
-//  }
-//
-//  }
+
+  void pinB_ISR(){
+  if(!fastDigitalRead(pinA)){
+    counter++; //Increments the times that pin B leads
+  }
+  counterB++;
+  }
 
 
 
@@ -79,48 +83,28 @@ void pinA_ISR() {
 
 
 void loop() {
+  if(abs(counterA - counterB) > 2){
+    digitalWrite(13, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(13, LOW);
+    counterA = counterB;
+  }
   VESCrpm = (rpm - 127) / 18.1428;
   targetEncoderPosition = (int)(targetInDegrees * 1.111111);
   distanceToTarget = targetEncoderPosition - counter;
-  //rollingAvg = rollingAvg * .99 + counter * .01;
-  //Serial.println(distanceToTarget);
-  /*
-    if(stepper%10 == 0)
-    {
-    Serial.print("Current Position: ");
-    Serial.println(counter);
-    Serial.print("Destination: ");
-    Serial.println(targetEncoderPosition);
-    }
-  */
-  //    VescUartSetCurrent(VESCrpm);
-
-  /*
-    if (Wire.available())
-    {
-    I2C_readAnything(targetEncoderPosition);
-    }
-  */
-
-
-  //    if(abs(distanceToTarget) > 7)
-  //    {
-  //      digitalWrite(13, HIGH);
-  //    }
-
+  
   if (distanceToTarget > 0) {
-    digitalWrite(pin_DIR, HIGH);
+    fastDigitalWrite(pin_DIR, HIGH);
   }
   else {
-    digitalWrite(pin_DIR, LOW);
+   fastDigitalWrite(pin_DIR, LOW);
   }
   
-    if (abs(distanceToTarget) > 1) {
+    if (abs(distanceToTarget) > 4) {
 
-      digitalWrite(pin_STEP, HIGH);
-      delayMicroseconds(1);
-      digitalWrite(pin_STEP, LOW);
-      lastPulseMicros = micros();
+      fastDigitalWrite(pin_STEP, HIGH);
+      delayMicroseconds(5);
+      fastDigitalWrite(pin_STEP, LOW);
     }
-    delayMicroseconds(999);
+    delayMicroseconds(250);
 }
